96928292a1e28021c0d99de5c1ee228d1abbab53
battusairam <58307306+battusairam@users.noreply.github.com>
2023-06-16 16:31:41 +0530

    Password Rule1

---
+++ UC6-Password-Rule1.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace RegexUserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Email address: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Enter Mobile number: ");
+            string mobileNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter Password: ");
+            string password = Console.ReadLine();
+
+            string namePattern = "^[A-Z][a-zA-Z]{2,}$";
+            string emailPattern = @"^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$";
+            string mobilePattern = @"^\d{2}\s\d{10}$";
+            string passwordPattern = @"^.{8,}$";
+
+            bool isValidFirstName = Regex.IsMatch(firstName, namePattern);
+            bool isValidLastName = Regex.IsMatch(lastName, namePattern);
+            bool isValidEmail = Regex.IsMatch(email, emailPattern);
+            bool isValidMobileNumber = Regex.IsMatch(mobileNumber, mobilePattern);
+            bool isValidPassword = Regex.IsMatch(password, passwordPattern);
+
+            if (isValidFirstName)
+            {
+                Console.WriteLine("Valid first name.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name.");
+            }
+
+            if (isValidLastName)
+            {
+                Console.WriteLine("Valid last name.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid last name.");
+            }
+
+            if (isValidEmail)
+            {
+                Console.WriteLine("Valid email address.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email address.");
+            }
+
+            if (isValidMobileNumber)
+            {
+                Console.WriteLine("Valid mobile number.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid mobile number.");
+            }
+
+            if (isValidPassword)
+            {
+                Console.WriteLine("Valid password.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid password.");
+            }
+        }
+    }
+}
